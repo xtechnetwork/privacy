76fcc514d9e083afbe78da07da09708ecc920d26
xtechnetwork <114317537+xtechnetwork@users.noreply.github.com>
2023-06-29 02:01:52 +0800

    Create utility.sh

---
+++ utility.sh
@@ -0,0 +1,18 @@
+apt-get install nano wget curl zip unzip tar gzip p7zip-full bc rc openssl cron net-tools dnsutils dos2unix screen bzip2 ccrypt -y
+apt-get install ca-certificates nginx ruby apt-transport-https lsb-release -y
+apt-get remove --purge ufw firewalld -y
+apt-get autoremove -y
+apt update
+apt upgrade -y
+apt-get upgrade -y
+apt install dnsutils jq -y
+apt-get install net-tools -y
+apt-get install tcpdump -y
+apt-get install dsniff -y
+apt install grepcidr -y
+apt-get install figlet -y
+apt-get install iptables-persistent
+sudo apt-get ssl-cert bind9-utils install bzip2 gzip coreutils wget screen rsyslog iftop htop -y
+sudo apt-get install -y net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 
+sudo apt-get install -y bc apt-transport-https build-essential dirmngr libxml-parser-perl git
+apt-get install -y --fix-missing 
